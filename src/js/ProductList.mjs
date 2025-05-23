e395ff172706eb496f7e0967353a5d0680548e82
sferran2 <sferran@byupathway.edu>
2025-05-23 10:27:00 -0400

    Add search box

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,4 +1,4 @@
-import { renderListWithTemplate } from "./utils.mjs";
+import { renderListWithTemplate, getParams } from "./utils.mjs";
 
 function productCardTemplate(product) {
   const discounted = product.FinalPrice < product.SuggestedRetailPrice;
@@ -25,9 +25,44 @@ export default class ProductList {
   }
   async init() {
     const list = await this.dataSource.getData();
-    this.renderList(list);
+    const params = getParams()
+
+    let filteredList = list;
+    if (params.search) {
+      const query = params.search.toLowerCase();
+
+      filteredList = list.filter(product =>
+        product.Name.toLowerCase().includes(query) ||
+        product.Brand.Name.toLowerCase().includes(query)
+      );
+    }
+
+    this.renderList(filteredList);
   }
   renderList(list) {
-    renderListWithTemplate(productCardTemplate, this.listElement, list);
+    if (list.length === 0) {
+      this.listElement.innerHTML = `
+      <p class="no-results">No products found matching your search. </p>
+      <button id="reset-search" class="reset-button">Back to All Products</button>
+      `;
+    } else {
+      renderListWithTemplate(productCardTemplate, this.listElement, list);
+
+      const params = getParams();
+      if (params.search) {
+        const resetBtn = document.createElement("button");
+        resetBtn.textContent = "Back to All Products";
+        resetBtn.id = "reset-search";
+        resetBtn.classList.add("reset-button");
+        this.listElement.insertAdjacentElement("afterend", resetBtn);
+      }
+    }
+
+    const resetButton = document.getElementById("reset-search");
+    if (resetButton) {
+      resetButton.addEventListener("click", () => {
+        window.location.href = "/src/index.html";
+      });
+    }
   }
-}
+}   
