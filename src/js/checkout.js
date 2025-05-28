6f5b5bf18e55feea4423837977bc417789cbded6
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2025-05-28 12:24:26 -0600

    fixed init() and removed commented code

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,39 +1,19 @@
-// import { loadHeaderFooter } from "./utils.mjs";
-// import CheckoutProcess from "./CheckoutProcess.mjs";
-// import Alert from "./Alert.js";
-
-// loadHeaderFooter();
-
-// new Alert().init();
-
-// const order = new CheckoutProcess("so-cart", ".checkout-summary");
-// order.init();
-
-// document
-//   .querySelector("#zip")
-//   .addEventListener("blur", order.calculateOrderTotal.bind(order));
-
-// document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
-//   e.preventDefault();
-//   order.checkout();
-// });
-
 import { loadHeaderFooter } from "./utils.mjs";
 import CheckoutProcess from "./CheckoutProcess.mjs";
+import Alert from "./Alert.js";
 
 loadHeaderFooter();
+
+new Alert().init();
 
 const order = new CheckoutProcess("so-cart", ".checkout-summary");
 order.init();
 
-// Add event listeners to fire calculateOrderTotal when the user changes the zip code
 document
   .querySelector("#zip")
   .addEventListener("blur", order.calculateOrderTotal.bind(order));
 
-// listening for click on the button
 document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
   e.preventDefault();
-
   order.checkout();
 });
