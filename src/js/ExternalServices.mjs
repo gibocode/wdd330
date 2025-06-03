1cfbfde98da5b68f29467fdb6f11d34b3836393d
Gilbor Camporazo Jr. <gilborcamporazojr@programmer.net>
2025-06-03 21:40:00 +0800

    Fix not updated file.

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -15,33 +15,35 @@ export default class ExternalServices {
     // this.category = category;
     // this.path = `../json/${this.category}.json`;
   }
-  
   async getData(category) {
     const response = await fetch(`${baseURL}products/search/${category}`);
     const data = await convertToJson(response);
+
     return data.Result;
   }
-  
   async findProductById(id) {
     const response = await fetch(`${baseURL}product/${id}`);
     const data = await convertToJson(response);
     return data.Result;
   }
-  
+
   async checkout(payload) {
-    
+
     let url = "http://wdd330-backend.onrender.com/checkout";
     let h = new Headers();
     h.append("Content-Type", "application/json");
-    
+
     const response = await fetch(url, {
       method: "POST",
       headers: h,
       body: JSON.stringify(payload),
-    };
-    return await fetch(`${baseURL}checkout/`, options).then(convertToJson);
+    });
+
+    const data = await convertToJson(response);
+    return data.Result;
+
   }
-  
+
   async getAllProducts() {
     const categories = ["tents", "backpacks", "sleeping-bags", "hammocks"];
     const allProducts = [];
@@ -53,5 +55,5 @@ export default class ExternalServices {
     }
 
     return allProducts;
-  }
+    }
 }
